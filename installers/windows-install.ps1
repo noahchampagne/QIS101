2830bd57012661001569919df1494c023818c883
David Biersach <dbiersach@gmail.com>
2022-01-29 17:50:20 -0500

    Initial commit

---
+++ installers/windows-install.ps1
@@ -0,0 +1,18 @@
+Set-Location $HOME
+conda update -y -n base conda
+conda config --add channels conda-forge
+conda config --set channel_priority strict
+conda create -y -n qis101 python=3.9
+conda activate qis101
+conda install -y matplotlib sympy scipy scikit-learn
+conda install -y pandas pandasql docutils pandocfilters
+conda install -y numexpr h5py traitsui zipp pathspec openpyxl
+conda install -y pylint autopep8 black
+conda install -y websockets requests pyserial
+conda install -y ipython ipykernel ipympl
+conda install -y jupyterlab jupyterlab_code_formatter
+conda install -y numba
+pip install pygame
+pip install 'qiskit[all]'
+pip install 'qiskit[visualization]'
+conda update -y --all

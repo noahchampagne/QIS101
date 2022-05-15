86a5606083a2dd09f952d7ea06c878a7b88a1f27
David Biersach <dbiersach@gmail.com>
2022-05-15 17:51:39 -0400

    Initial commit

--- installers/windows-install.ps1
+++ installers/windows-install.ps1
@@ -1,18 +1,35 @@
 Set-Location $HOME
 conda update -y -n base conda
-conda config --add channels conda-forge
-conda config --set channel_priority strict
-conda create -y -n qis101 python=3.9
+conda create -y -n qis101 python=3.10
 conda activate qis101
-conda install -y matplotlib sympy scipy scikit-learn
-conda install -y pandas pandasql docutils pandocfilters
-conda install -y numexpr h5py traitsui zipp pathspec openpyxl
-conda install -y pylint autopep8 black
-conda install -y websockets requests pyserial
-conda install -y ipython ipykernel ipympl
-conda install -y jupyterlab jupyterlab_code_formatter
+conda config --env --add channels conda-forge
+conda config --env --set channel_priority strict
+conda install -y mayavi
+conda install -y numpy
 conda install -y numba
+conda install -y matplotlib
+conda install -y sympy
+conda install -y scipy
+conda install -y scikit-learn
+conda install -y pandas
+conda install -y pandasql
+conda install -y docutils
+conda install -y pandocfilters
+conda install -y numexpr
+conda install -y h5py
+conda install -y traitsui
+conda install -y pathspec
+conda install -y openpyxl
+conda install -y pylint
+conda install -y autopep8
+conda install -y black
+conda install -y websockets
+conda install -y requests
+conda install -y pyserial
+conda install -y nodejs
+conda install -y jupyterlab
+conda install -y jupyterlab_widgets
+conda install -y jupyterlab_code_formatter
+conda install -y ipympl
 pip install pygame
 pip install 'qiskit[all]'
-pip install 'qiskit[visualization]'
-conda update -y --all

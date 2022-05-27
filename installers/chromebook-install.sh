dc4a0cf2589a6c98f6b5bbc3f229f5be85de72c0
David Biersach <dbiersach@gmail.com>
2022-05-26 23:13:42 -0400

    Add PuLP and VSCode Extensions

--- installers/chromebook-install.sh
+++ installers/chromebook-install.sh
@@ -11,6 +11,7 @@ conda install -y matplotlib
 conda install -y sympy
 conda install -y scipy
 conda install -y scikit-learn
+conda install -y pulp
 conda install -y pandas
 conda install -y pandasql
 conda install -y docutils
@@ -33,6 +34,23 @@ conda install -y jupyterlab_code_formatter
 conda install -y ipympl
 pip install pygame
 pip install 'qiskit[all]'
+code --install-extension ms-code.cpptools
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension ms-vscode.powershell
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension visualstudioexptteam.vscodeintellicode
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension ms-vscode.cmake-tools
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension esbenp.prettier-vscode
+code --install-extension mechatroner.rainbow-csv
+code --install-extension emmanuelbeziat.vscode-great-icons
+code --install-extension james-yu.latex-workshop
+code --install-extension cschlosser.doxdocgen
+code --install-extension redhat.vscode-yaml
+code --install-extension eamodio.gitlens
 echo 'y' | jupyter lab --generate-config
 echo 'c.ServerApp.use_redirect_file = False' >> $HOME/.jupyter/jupyter_lab_config.py
 

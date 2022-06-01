e5808669871e78f41ed6023913c436fb7f50824d
David Biersach <dbiersach@gmail.com>
2022-06-01 02:40:45 -0400

    Fix description of totient

--- labs/Session 03 - Introducing Python/euler_totient.py
+++ labs/Session 03 - Introducing Python/euler_totient.py
@@ -18,7 +18,7 @@ def totient(n: int) -> int:
 
 def main():
     print("Natural numbers between 2 and 100", end=" ")
-    print("that are equal to their own totient:")
+    print("that exceed their totient by one:")
 
     for n in range(2, 101):
         if n == totient(n) + 1:

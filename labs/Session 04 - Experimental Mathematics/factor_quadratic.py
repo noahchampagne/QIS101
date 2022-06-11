c745e958edd20d2cf39d0ecaa26d9cee0d01808b
David Biersach <dbiersach@gmail.com>
2022-06-10 23:43:18 -0400

    Clean up output

--- labs/Session 04 - Experimental Mathematics/factor_quadratic.py
+++ labs/Session 04 - Experimental Mathematics/factor_quadratic.py
@@ -3,9 +3,8 @@
 
 
 def factor_quadratic(J, K, L):
-    print("Given the quadratic:")
-    print(f" {J}x^2 + {K}x + {L} = 0")
-    print("The factors are:")
+    print("Given the quadratic:", end=" ")
+    print(f"{J}x^2 + {K}x + {L}")
 
     for a in range(1, J + 1):
         if J % a == 0:
@@ -14,12 +13,12 @@ def factor_quadratic(J, K, L):
                 if L % b == 0:
                     d = L // b
                     if a * d + b * c == K:
-                        print(f" ({a}x + {b})" f"({c}x + {d})")
+                        print("The factors are:", end=" ")
+                        print(f"({a}x + {b})" f"({c}x + {d})")
 
 
 def main():
     factor_quadratic(115425, 3254121, 379020)
-    factor_quadratic(115425, 3254121, 379021)
 
 
 if __name__ == "__main__":

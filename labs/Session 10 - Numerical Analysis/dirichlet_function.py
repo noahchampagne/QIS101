9398e570dbc8a296bc5487f782c9986288b503e8
David Biersach <dbiersach@gmail.com>
2022-06-12 02:01:35 -0400

    Initial commit

--- labs/Session 10 - Numerical Analysis/dirichlet_function.py
+++ labs/Session 10 - Numerical Analysis/dirichlet_function.py
@@ -4,20 +4,20 @@
 import mpmath
 
 mpmath.mp.dps = 2000  # dps = decimal places
-k = 100
-n = 1e10
 
 
 def dirichlet_function(x):
-    d = mpmath.power(mpmath.cos(mpmath.factorial(k) * mpmath.pi * x), n)
-    return mpmath.chop(d)
+    k = 100
+    n = 1e10
+    f = mpmath.power(mpmath.cos(mpmath.factorial(k) * mpmath.pi * x), n)
+    return mpmath.chop(f)
 
 
 def main():
-    print(dirichlet_function(2))
-    print(dirichlet_function(2.5))
-    print(dirichlet_function(mpmath.sqrt(2)))
-    print(dirichlet_function(mpmath.e))
+    print(f"f(2) = {dirichlet_function(2)}")
+    print(f"f(2.5) = {dirichlet_function(2.5)}")
+    print(f"f(sqrt(2)) = {dirichlet_function(mpmath.sqrt(2))}")
+    print(f"f(e) = {dirichlet_function(mpmath.e)}")
 
 
 if __name__ == "__main__":

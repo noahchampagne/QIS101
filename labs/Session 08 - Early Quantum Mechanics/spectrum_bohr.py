c67abd16a7aeff675cc5f453cb825f55a963d19e
David Biersach <dbiersach@gmail.com>
2022-06-14 00:59:06 -0400

    Bug fix in Bohr Model

--- labs/Session 08 - Early Quantum Mechanics/spectrum_bohr.py
+++ labs/Session 08 - Early Quantum Mechanics/spectrum_bohr.py
@@ -3,25 +3,25 @@
 
 
 def main():
-    eCharge = 1.6e-19
-    eMass = 9.1e-31
-    permittivity = 8.84e-12
-    hPlank = 6.63e-34
-    speedLight = 3e8
+    eCharge = 1.602e-19
+    eMass = 9.109e-31
+    permittivity = 8.854e-12
+    hPlank = 6.626e-34
+    speedLight = 2.998e8
 
     # Bohr's formula for ground state energy
     E0 = (pow(eCharge, 4) * eMass) / (8 * pow(permittivity, 2) * pow(hPlank, 2))
 
     print("Bohr Model Hydrogen Spectral Lines")
 
-    for i in range(1, 5):
-        for f in range(i + 1, i + 6):
+    for f in range(1, 5):
+        for i in range(f + 1, f + 6):
             # Initial energy level
             Ei = -E0 / pow(i, 2)
             # Final energy level
             Ef = -E0 / pow(f, 2)
             # Formula for waveLength in nanometers
-            waveLength = hPlank * speedLight / (Ef - Ei) * 1e9
+            waveLength = hPlank * speedLight / (Ei - Ef) * 1e9
             print(f"{f:>3,}{waveLength:10.0f} nm")
         print()
 

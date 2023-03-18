416429dc47ad491e20eb1cd79363cbe32e1c8157
David Biersach <dbiersach@gmail.com>
2023-03-18 18:14:29 -0400

    Add typing

--- labs/Session 08 - Early Quantum Mechanics/spectrum_rydberg.py
+++ labs/Session 08 - Early Quantum Mechanics/spectrum_rydberg.py
@@ -2,15 +2,17 @@
 """spectrum_rydberg.py"""
 
 
-def main():
-    rydberg_constant = 1.0967757e7
+def main() -> None:
+    rydberg_constant: float = 1.0967757e7
 
     print("Rydberg Formula for Hydrogen Spectral Lines")
 
     for k in range(1, 5):
         for j in range(k + 1, k + 6):
             # Formula for waveLength in nanometers
-            wave_length = 1 / (rydberg_constant * (1 / pow(k, 2) - 1 / pow(j, 2))) * 1e9
+            wave_length: float = (
+                1 / (rydberg_constant * (1 / pow(k, 2) - 1 / pow(j, 2))) * 1e9
+            )
             print(f"\t{j:>2} -> {k:>2}{wave_length:8.0f} nm")
         print()
 

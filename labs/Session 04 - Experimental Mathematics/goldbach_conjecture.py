c58bcf5f29ffdac66ed0ccd18ec7f7ee53fb1c20
David Biersach <dbiersach@gmail.com>
2023-04-11 00:59:50 -0400

    Switch to NumPy arrays

--- labs/Session 04 - Experimental Mathematics/goldbach_conjecture.py
+++ labs/Session 04 - Experimental Mathematics/goldbach_conjecture.py
@@ -5,10 +5,8 @@ from __future__ import annotations
 
 import typing
 from itertools import combinations_with_replacement
-from typing import cast
 
 import numpy as np
-from numpy.typing import ArrayLike  # type: ignore
 from sympy import prime  # type: ignore
 
 if typing.TYPE_CHECKING:
@@ -22,18 +20,19 @@ def main() -> None:
     print("Verifying Goldbach's conjecture for every even ", end="")
     print(f"integer from 6 to {test_limit} inclusive:")
 
-    # Use a list comprehension to generate a list of first 'n' primes
-    primes: list[int] = cast(list[int],
-                              [prime(n) for n in range(2, test_limit)])  # type: ignore
+    # Use a list comprehension to create an array of the first 'n' primes
+    primes: NDArray[np.int_] = np.array(
+        [prime(n) for n in range(2, test_limit)]  # type: ignore
+    )
 
     # Generate all pairs of primes (with repetition)
     prime_pairs: list[tuple[int, ...]] = [
         *combinations_with_replacement(primes, 2)  # type: ignore
     ]
 
-    # Create sorted list containing the sum of each pairwise primes
-    summed_pairs: NDArray[np.float_] = np.sort(
-        cast(ArrayLike, [sum(pair) for pair in prime_pairs])
+    # Create sorted array containing the unique sums of each prime pairs
+    summed_pairs: NDArray[np.int_] = np.sort(
+        np.unique(np.array([sum(pair) for pair in prime_pairs]))
     )
 
     # Determine which EVEN integers from 6 to n (inclusive)
